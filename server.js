8dd973a3ba9b80763d81e84f628439cb56978dc1
flynn-28 <107883034+flynn-28@users.noreply.github.com>
2024-10-22 09:41:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,24 +2,22 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const bodyParser = require('body-parser');
 const url = require('url');
-const dotenv = require('dotenv');
-
-dotenv.config();
+const responseRewrite = require('response-rewrite');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send(`
-    <form method="POST" action="/unblock">
-      <label for="url">Enter URL:</label>
-      <input type="text" placeholder="https://google.com" name="url" id="url" required>
+    <form method="POST" action="/browse">
+      <label for="url">Enter URL to unblock:</label>
+      <input type="text" name="url" id="url" required>
       <button type="submit">Go</button>
     </form>
   `);
 });
 
-app.post('/unblock', (req, res) => {
+app.post('/browse', (req, res) => {
   const targetUrl = req.body.url;
   if (!/^https?:\/\//.test(targetUrl)) {
     return res.status(400).send('Invalid URL. Please include http:// or https://');
@@ -27,6 +25,24 @@ app.post('/unblock', (req, res) => {
 
   res.redirect(`/proxy?url=${encodeURIComponent(targetUrl)}`);
 });
+
+const rewriteUrls = (proxyRes, req, res) => {
+  const contentType = proxyRes.headers['content-type'] || '';
+  if (contentType.includes('text/html')) {
+    let buffer = [];
+    proxyRes.on('data', (chunk) => buffer.push(chunk));
+    proxyRes.on('end', () => {
+      let body = Buffer.concat(buffer).toString();
+      
+      body = body.replace(/(href|src)="(\/[^"]*)"/g, (match, p1, p2) => {
+        return `${p1}="/proxy?url=${encodeURIComponent(req.query.url)}${p2}"`;
+      });
+
+      res.setHeader('content-type', 'text/html');
+      res.send(body);
+    });
+  }
+};
 
 app.use('/proxy', (req, res, next) => {
   const targetUrl = req.query.url;
@@ -39,18 +55,20 @@ app.use('/proxy', (req, res, next) => {
   const proxy = createProxyMiddleware({
     target: `${parsedUrl.protocol}//${parsedUrl.host}`,
     changeOrigin: true,
+    selfHandleResponse: true, 
+    onProxyRes: (proxyRes, req, res) => {
+      rewriteUrls(proxyRes, req, res);
+    },
     pathRewrite: {
-      '^/proxy': '',
+      '^/proxy': '', 
     },
     logLevel: 'debug',
   });
 
-  proxy(req, res, next);
+  proxy(req, res, next); 
 });
 
-const port = process.env.PORT;
-const host = process.env.HOST ;
-
-app.listen(port, host, () => {
-  console.log(`Server running on http://${host}:${port}`);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
